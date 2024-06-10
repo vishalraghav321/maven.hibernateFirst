4b158b56b5058732b137ed72ba74df580c3365a0
vishalraghav321 <vishalraghav321@gmail.com>
2024-06-10 17:01:42 +0530

    update

--- src/main/java/hibernateFirst/App.java
+++ src/main/java/hibernateFirst/App.java
@@ -3,6 +3,9 @@ package hibernateFirst;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 import hibernateFirst.entity.Employee;
@@ -10,9 +13,17 @@ import hibernateFirst.entity.Employee;
 public class App {
 
 	public static void main(String[] args) {
-		Employee emp = new Employee(1,"Vishal","Male",250000);
+		Employee emp = new Employee(3,"Raghav","Male",250000);
+//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+//		SessionFactory sessionFactory = cfg.buildSessionFactory();
+//		Session session = sessionFactory.openSession();
 		
-		
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+		SessionFactory sessionFactory = new MetadataSources(ssr).getMetadataBuilder().build().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.persist(emp);
+		tx.commit();
 	}
 
 }
