d3f49fd477de18a35b8d9490af5b239f2d655005
vishalraghav321 <vishalraghav321@gmail.com>
2024-06-11 17:07:45 +0530

    update

--- src/main/java/hibernateFirst/client/App.java
+++ src/main/java/hibernateFirst/client/App.java
@@ -1,4 +1,4 @@
-package hibernateFirst;
+package hibernateFirst.client;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -13,11 +13,11 @@ import hibernateFirst.entity.Employee;
 public class App {
 
 	public static void main(String[] args) {
-		Employee emp = new Employee(3,"Raghav","Male",250000);
+		Employee emp = new Employee("Abhay", "Male", 250000);
 //		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
 //		SessionFactory sessionFactory = cfg.buildSessionFactory();
 //		Session session = sessionFactory.openSession();
-		
+
 		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
 		SessionFactory sessionFactory = new MetadataSources(ssr).getMetadataBuilder().build().buildSessionFactory();
 		Session session = sessionFactory.openSession();
--- src/main/java/hibernateFirst/entity/Employee.java
+++ src/main/java/hibernateFirst/entity/Employee.java
@@ -5,9 +5,9 @@ public class Employee {
 	private String name;
 	private String gender;
 	private int salary;
-	public Employee(int id, String name, String gender, int salary) {
+	public Employee(String name, String gender, int salary) {
 		super();
-		this.id = id;
+//		this.id = id;
 		this.name = name;
 		this.gender = gender;
 		this.salary = salary;
