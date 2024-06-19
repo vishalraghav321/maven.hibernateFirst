a8cafc9317c102d8af5ac8f6db8f9fb1b053e9ea
vishalraghav321 <vishalraghav321@gmail.com>
2024-06-19 16:46:52 +0530

    update

--- src/main/java/hibernateFirst/entity/Employee.java
+++ src/main/java/hibernateFirst/entity/Employee.java
@@ -1,11 +1,14 @@
 package hibernateFirst.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity (name = "TCS_emp")
+@Entity (name = "Employee")
 public class Employee {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String gender;
